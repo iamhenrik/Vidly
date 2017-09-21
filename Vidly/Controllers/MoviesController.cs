a7c04c9ad014632a6a9f8220f1320a6800708a17
Henrik Farstad <henfar11@hotmail.com>
2017-09-22 01:27:37 +0200

    fortsatte med tutorial

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -9,14 +9,41 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        /*
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12}")]
+		public ActionResult ByReleaseDate(int year, int month)
+		{
+			return Content(year + "/" + month);
+		}
+        */
         // GET: Movies
+
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
 
-            return Content("hello world!");
+
+            return View(movie);
+            //return Content("hello world!");
             //return HttpNotFound();
             //return RedirectToAction("Index", "Home", new {page = 1, sortBy="name"});
         }
+        /*
+        public ActionResult Edit(int Id)
+        {
+            return Content("Id=" + Id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+        */
+
     }
 }
